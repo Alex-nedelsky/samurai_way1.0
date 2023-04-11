a10c655e404010b07e98043697e04b83534c116e
Alex-nedelsky <alex.nedelsky@gmail.com>
2023-04-11 23:25:46 +0200

    lesson_9

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
 import React from 'react';
 import './App.css';
+import Technologies from "./Technologies";
+import Header from "./Header";
 
-
-const App = () => {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+function App() {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
 }
 
-const Technologies = () => {
-  return (
-      <div>
-          <ul>
-              <li>css</li>
-              <li>html</li>
-              <li>js</li>
-              <li>react</li>
-          </ul>
-      </div>
-  )
-}
-
-
-const Header = () => {
-
-}
 
 export default App;
---
+++ src/Technologies.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Technologies() {
+    // @ts-ignore
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                < li>html</li>
+                < li>js</li>
+                < li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Technologies;
