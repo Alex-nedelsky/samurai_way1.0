950c5e1560ebb943d37846b5f2a48e9a4bbe60c4
Alex-nedelsky <alex.nedelsky@gmail.com>
2023-04-12 23:55:13 +0200

    lesson_14

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import exp from "constants";
+import s from './Header.module.css'
+
 
 function Header() {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img src="https://www.logodesign.net/logo/crossed-steel-pillars-2472ld.png"></img>
         </header>
     )
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import s from './Profile.module.css'
 function Profile() {
     return(
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://w.forfun.com/fetch/1f/1f2f6017fe992dd00493d3cda4d808fe.jpeg"/>
             </div>
@@ -18,11 +18,11 @@ function Profile() {
             <div>
                 old_post
             </div>
-            <div>
-                <div>
+            <div className={s.posts}>
+                <div className={s.item}>
                     post_1
                 </div>
-                <div>
+                <div className={s.item}>
                     post_2
                 </div>
             </div>
