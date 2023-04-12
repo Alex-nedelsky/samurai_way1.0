02275cb110299071629c1d4e8446edd453b068fa
Alex-nedelsky <alex.nedelsky@gmail.com>
2023-04-12 22:10:09 +0200

    lesson_12

---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import exp from "constants";
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://www.logodesign.net/logo/crossed-steel-pillars-2472ld.png"></img>
+        </header>
+    )
+
+}
+
+
+export default Header;
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Profile() {
+    return(
+        <div className='content'>
+            <div>
+                <img src="https://w.forfun.com/fetch/1f/1f2f6017fe992dd00493d3cda4d808fe.jpeg"/>
+            </div>
+            <div>
+                ava+descript
+            </div>
+            <div>
+                my posts
+            </div>
+            <div>
+                new_post
+            </div>
+            <div>
+                old_post
+            </div>
+            <div>
+                <div>
+                    post_1
+                </div>
+                <div>
+                    post_2
+                </div>
+            </div>
+        </div>
+
+    )
+
+}
+
+export default Profile
